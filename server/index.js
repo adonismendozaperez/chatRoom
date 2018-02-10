6b11db56b540228c68cfe8711fe5c7fe8035b444
Adonis <adonis.mendoza@outlook.com>
2018-02-09 23:26:01 -0400

    [ADD] conneting with socket

--- server/index.js
+++ server/index.js
@@ -4,16 +4,18 @@ const express = require('express'),
       io      = require("socket.io")(server);
 
 
+/* GET PAGE FOR DEFAULT */
+app.use(express.static('client'));
+
 app.get('/home',function (req,resp){
     resp.status(200).send('Test');
 });
 
-
-
+/* CONNET TO SOCKET */
+io.on('connection', function (socket){
+    console.log(`client ip: ${socket.handshake.address} is connecting...`);
+});
 
 server.listen(2122,function (){
     console.log('Server On!');
 });
-
-
-
