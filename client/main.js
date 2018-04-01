223bcd0b6f8600c0a8461de152fd95257722d41f
Adonis <adonis.mendoza@outlook.com>
2018-04-01 12:10:58 -0400

    [ADD] beautiful design

--- client/main.js
+++ client/main.js
@@ -1,23 +1,21 @@
 let socket = io.connect('http://172.22.119.193:2122',{'forceNew':true});
 $(document).ready(overlay);
 
-socket.on('messages', function(data){
+socket.on('messages', (data)=>{
     console.log(data);
     render(data);
 });
  
 function render(data){
-    let html = data.map(function(message, index){
+    let html = data.map((message, index)=>{
         return (`
             <div class="message">
-                <strong>${message.nickname}</strong> dice:
+                <strong>${message.nickname}</strong> say:
                 <p>${message.text}</p>
             </div>
         `);
     }).join(' ');
     
-//    let nick= document.querySelector('#nick');
-//     nick.appendChild(data.nickname);
     console.log(data.nickname);
     let div_msgs = document.getElementById('messages');
     div_msgs.innerHTML = html;
@@ -26,14 +24,15 @@ function render(data){
  
 function addMessage(e){
     let message = {
-        nickname: document.getElementById('nickname').value,
-        text: document.getElementById('text').value
+        nickname: $('#nickname').val(),
+        text: $('#text').val()
     };
  
     // document.getElementById('nickname').style.display = 'none';
     document.getElementById('nickname').readOnly = true;
     socket.emit('add-message', message);
- 
+    $('#text').val('');
+    $('#text').focus();
     return false;
 }
 /* MODAL */
